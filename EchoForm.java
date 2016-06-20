03914817ae97210fe249b0b8482ed06f3d96599c
ShizuNaka <j_nara01090410@yahoo.co.jp>
2016-06-20 11:30:27 +0900

    変数nameにコメントを入れました。

--- EchoForm.java
+++ EchoForm.java
@@ -4,6 +4,7 @@ import org.seasar.struts.annotation.Required;
 import org.seasar.struts.annotation.Msg;
 
 public class EchoForm {
+	// ƒ†[ƒU‚Ì–¼
     @Required(msg=@Msg(key="errors.nameRequired"))
     public String name;
 
